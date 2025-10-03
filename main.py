0b5fcf05ca635698df3074c64502ed963c103b0d
caizp32 <caizhipeng32@gmail.com>
2025-10-03 11:25:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Form, Request
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 import os
-import browser_cookie3
+#import browser_cookie3
 from yt_dlp import YoutubeDL
 
 app = FastAPI()
@@ -11,7 +11,7 @@ os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 templates = Jinja2Templates(directory="templates")
 # 自动从浏览器获取 Cookie（支持 Chrome / Edge / Firefox）
-cookies = browser_cookie3.chrome()
+#cookies = browser_cookie3.chrome()    #'cookies': cookies,   # 直接传入 cookie 对象
 
 def download_youtube(url: str) -> str:
     ydl_opts = {
@@ -21,8 +21,7 @@ def download_youtube(url: str) -> str:
         'format': 'best[ext=mp4]', # Try a more general MP4 format 
         'outtmpl': '%(title)s.%(ext)s', 
         'noplaylist': True, # Download only the video, not the entire playlist
-        'cookiefile': 'cookies.txt',   # 不需要手动写文件
-        #'cookies': cookies,   # 直接传入 cookie 对象
+        'cookiefile': 'cookies.txt'   # 不需要手动写文件
     }
     with YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=True)
